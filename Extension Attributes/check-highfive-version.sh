e22abdbb370be21b1a9f21ef922b692b0fdb041c
Raul Santos <raul@hioscar.com>
2017-04-18 11:28:19 -0400

    Updated user

--- Extension Attributes/check-highfive-version.sh
+++ Extension Attributes/check-highfive-version.sh
@@ -2,7 +2,7 @@
 loggedInUser="$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')"
 
 # Check user
-if [ "$loggedInUser" = "oscadmin" ]; then
+if [ "$loggedInUser" = "admin" ]; then
     echo "Admin logged in, Dont run Login Script"
     exit 0
 elif [ "$loggedInUser" = "" ]; then
